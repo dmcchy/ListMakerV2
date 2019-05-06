8be486550545d576aa5c0ca0b6c7e4ee0bf437dd
Demby Abella <dmcchy@gmail.com>
2019-05-06 16:35:15 +0800

    ++ Finished adding task input in ListDetailActivity

--- app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
@@ -2,11 +2,16 @@ package com.raywenderlich.listmaker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.InputType
+import android.widget.EditText
 
 class ListDetailActivity : AppCompatActivity() {
 
+    lateinit var addTaskButton: FloatingActionButton
     lateinit var listItemsRecyclerView: RecyclerView
     lateinit var list: TaskList
 
@@ -14,11 +19,38 @@ class ListDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_detail)
 
+        addTaskButton = findViewById<FloatingActionButton>(R.id.add_task_button)
+        addTaskButton.setOnClickListener {
+            showCreateTaskDialog()
+        }
+
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)
         title = list.name
 
         listItemsRecyclerView = findViewById<RecyclerView>(R.id.list_items_recyclerview)
         listItemsRecyclerView.adapter = ListItemsRecyclerViewAdapter(list)
         listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    // Modal to add new items.
+    private fun showCreateTaskDialog() {
+        val taskEditText = EditText(this)
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText) // You can assign view that were created imperatively
+            .setPositiveButton(R.string.add_task) { dialog, _ ->
+                val task = taskEditText.text.toString()
+                list.tasks.add(task)
+
+                val recyclerViewAdapter = listItemsRecyclerView.adapter as
+                        ListItemsRecyclerViewAdapter
+                recyclerViewAdapter.notifyItemInserted(list.tasks.size)
+
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 }
